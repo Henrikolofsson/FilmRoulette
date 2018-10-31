ebe85cd264b07e107e7925407c3c67c3bd3aab30
lundow <eriklundow@gmail.com>
2018-10-31 18:06:02 +0100

    Ny push

--- app/src/main/java/henrik/mau/p3/MDB_API.java
+++ app/src/main/java/henrik/mau/p3/MDB_API.java
@@ -17,18 +17,30 @@ import java.util.Arrays;
 
 public class MDB_API {
     private static ArrayList<String> posters;
+    private static ArrayList<String> overviews;
+    private static ArrayList<String> titles;
+    private static ArrayList<String> dates;
+    private static ArrayList<String> ratings;
+    private static ArrayList<String> youtubes;
+    private static ArrayList<String> youtubes2;
+    private static ArrayList<String> ids;
+    private static ArrayList<Boolean> favorited;
+    private static ArrayList<ArrayList<String>> comments;
+
     private static boolean sortByPop;
+
     private static String API_KEY = "1382cca4854a01eee1ddc83dc5102dbf";
     private MainActivity activity;
     private Controller controller;
+    private ArrayList<Movie> movies;
 
-    public MDB_API(MainActivity activity, Controller controller){
-        this.activity=activity;
-        this.controller=controller;
+    public MDB_API(MainActivity activity, Controller controller) {
+        this.activity = activity;
+        this.controller = controller;
 
     }
 
-    public  void startImageLoadTask() {
+    public void startImageLoadTask() {
         new ImageLoadTask().execute();
     }
 
@@ -45,7 +57,6 @@ public class MDB_API {
                     continue;
                 }
             }
-
         }
 
         @Override
@@ -110,10 +121,9 @@ public class MDB_API {
                         }
                     }
                 }
-
-
             }
         }
+
 
         public String[] getPathsFromJSON(String JSONStringParam) throws JSONException {
 
@@ -130,4 +140,26 @@ public class MDB_API {
             return result;
         }
     }
+
+
+    public String[] getStringsFromJSON(String JSONStringParam, String param) throws JSONException {
+        JSONObject JSONString = new JSONObject(JSONStringParam);
+
+        JSONArray moviesArray = JSONString.getJSONArray("results");
+        String[] result = new String[moviesArray.length()];
+
+        for (int i = 0; i < moviesArray.length(); i++) {
+            JSONObject movie = moviesArray.getJSONObject(i);
+            if (param.equals("vote_average")) {
+                Double number = movie.getDouble("vote_average");
+                String rating = Double.toString(number) + "/10";
+                result[i] = rating;
+            } else {
+                String data = movie.getString(param);
+                result[i] = data;
+            }
+        }
+        return result;
+    }
+
 }
---
+++ app/src/main/java/henrik/mau/p3/Movie.java
@@ -0,0 +1,13 @@
+package henrik.mau.p3;
+
+public class Movie {
+    private String title;
+    private String date;
+    private String overview;
+    private String rating;
+    private String id;
+    private String favorited;
+    private String comment;
+    private String youtube;
+
+}
